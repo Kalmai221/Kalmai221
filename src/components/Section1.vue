8296538abdca57a211901974d3b5953b3f87203a
Kalmai221 <hkurtislee@outlook.com>
2023-03-26 19:21:58 +0100

    Website Page Development

--- src/components/Section1.vue
+++ src/components/Section1.vue
@@ -9,7 +9,18 @@ defineProps({
 
 <template>
   <div class="section">
-    <h3>What do I know?</h3>
+    <h2>What do I know?</h2>
+    <br>
+    <div class="insection">
+      <h3>Website Development</h3>
+      <p>You can review the source code of this website at <a href="https://github.com/kalmai221/Kalmai221">Github</a> if you don't trust
+      me</p>
+    </div>
+    <br>
+    <div class="insection">
+      <h3>Discord Bot Development</h3>
+      <p>If you wish to view the bot in action, contact me on Discord at <b>Kalmai221#1905</b> and I can schedule a time for you to test the bot</p>
+    </div>
   </div>
 </template>
 
@@ -19,6 +30,15 @@ defineProps({
    border-radius: 5px;
    text-align: center;
    margin: 0 20 0 20;
+   border-style: solid;
+   border-width: 1px;
+ }
+ 
+ .insection {
+   border-color: #41B883;
+   border-radius: 5px;
+   text-align: center;
+   margin: 0 10 0 10;
    border-style: solid;
    border-width: 1px;
  }
