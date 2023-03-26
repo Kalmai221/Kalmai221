a717f63808947eb86397f017c8612adaaa854f53
Kalmai221 <hkurtislee@outlook.com>
2023-03-26 08:12:49 +0100

    Page Development

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import Section1 from './components/Section1.vue'
     <div class="wrapper">
       <Title msg="An Average Intermediate Developer" />
       <svg height="5" width="100%">
-        <line x1="0" y1="0" x2="20" y2="0"
+        <line x1="0" y1="0" x2="0" y2="0"
         style="stroke:rgb(128, 128, 128);stroke-width:1" />
        </svg>
     </div>
